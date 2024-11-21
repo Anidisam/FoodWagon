9cb5d0d77ed92f905411e02af22d99bfeccfbba6
anidisamuel2006@gmail.com <anidisamuel2006@gmail.com>
2024-11-21 10:04:07 -0800

    extra commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-import FoodCards from "./components/FoodCards"
+// import FoodCards from "./components/FoodCards"
 import Work from "./components/Work"
 import Popular from "./components/Popular"
-// import Featured from "./components/Featured"
+import Featured from "./components/Featured"
 import Best from "./components/Best"
 
 
@@ -14,10 +14,10 @@ function App() {
     <>
       <Navbar />
       <Hero />
-      <FoodCards />
+      {/* <FoodCards /> */}
       <Work />
       <Popular />
-      {/* <Featured /> */}
+      <Featured />
       <Best />
     </>
   )
