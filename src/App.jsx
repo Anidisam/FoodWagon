c77c49e01b971f61c69c6093ba7364b547a6ad5c
anidisamuel2006@gmail.com <anidisamuel2006@gmail.com>
2024-11-21 09:58:38 -0800

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from "./components/Hero"
 import FoodCards from "./components/FoodCards"
 import Work from "./components/Work"
 import Popular from "./components/Popular"
-import Featured from "./components/Featured"
+// import Featured from "./components/Featured"
 import Best from "./components/Best"
 
 
@@ -17,7 +17,7 @@ function App() {
       <FoodCards />
       <Work />
       <Popular />
-      <Featured />
+      {/* <Featured /> */}
       <Best />
     </>
   )
